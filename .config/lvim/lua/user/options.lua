4e857868f84f7cb57fdc420ce0de3d25c9ececb0
myamusashi <jinxprogilang666@gmail.com>
2024-01-19 13:20:27 +0700

    feat: add files for each plugins

--- .config/lvim/lua/user/options.lua
+++ .config/lvim/lua/user/options.lua
@@ -1,6 +1,6 @@
 lvim.format_on_save = true
 
-lvim.colorscheme = "one_monokai"
+lvim.colorscheme = "catppuccin-frappe"
 vim.o.background = "dark"
 
 vim.opt.number = true
@@ -12,11 +12,13 @@ vim.opt.list = false
 vim.opt.scrolloff = 0
 lvim.transparent_window = true
 lvim.use_icons = false
-vim.opt.guifont = "Hack Nerd Font Mono:h10"
+vim.opt.guifont = "Hack Nerd Font Mono:h12"
 lvim.use_icons = true
 vim.opt.termguicolors = true
 vim.o.autoindent = true
-vim.o.shiftwidth = 4
+vim.o.shiftwidth = 2
+
+vim.notify = require("telescope").load_extension("notify")
 
 -- Fold
 vim.o.foldcolumn = '1'
